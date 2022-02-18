86c492059ba883f785ecf0c52a6c56d945ab5c27
gaychin <gcyap1227@gmail.com>
2022-02-18 11:37:16 +0800

    Revert "added use ref2 hook and useLayoutEffect hook"
    This reverts commit e58e5a2faf40e1e95c688c8630529c8692e72621.
    
    e

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import UseEffect from "./Hooks/UseEffect";
 import UseContext from "./Hooks/UseContext";
 import PropDrilling from "./Hooks/PropDrilling";
 import UseRef1 from "./Hooks/UseRef1";
-import UseRef2 from "./Hooks/UseRef2"
-import UseLayoutEffect from "./Hooks/UseLayoutEffect"
 
 //Uncomment the hook that you want to learn, to see the demo
 function App() {
@@ -16,15 +14,10 @@ function App() {
           {/* <UseEffect /> */}
           {/* <PropDrilling/> */}
           {/* <UseContext/> */}
-          {/* <UseRef1/> */}
-          {/* <UseRef2/> */}
-          {/* <UseMemo/> */}
-          <UseLayoutEffect/>
+          <UseRef1/>
         </header>
       </div>
   );
 }
 
 export default App;
-
-
